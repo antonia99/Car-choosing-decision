33e270138c3ef821fb8b2d78b826e900900c6628
Antonia <57840544+antonia99@users.noreply.github.com>
2022-05-17 15:36:08 +0300

    Add files via upload

---
+++ decizie de alegere a unui automobil/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decizie_de_alegere_a_unui_automobil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cel mai mic pret al masinii:");
+            int pret= (int)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Cheltuieli de intretinere minime:");
+            int intretinereMin= (int)Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Cheltuieli de intretinere maxime:");
+            int intretinereMax = (int)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Grad de confort, nivel dotari, siguranta maxim(1-20):");
+            int confortMax = (int)Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Grad de confort, nivel dotari, siguranta minim(1-20):");
+            int confortMin = (int)Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
